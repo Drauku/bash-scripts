cd2ccd842979f95781c18a1507db7723752a674c
Drauku <drauku@gmail.com>
2023-08-02 22:08:53 -0500

    Update ipv6-generate.sh
    script wholly changed after extensive troubleshooting session with claude.ai which did not find a good solution

--- ipv6-generate.sh
+++ ipv6-generate.sh
@@ -1,19 +1,31 @@
 #!/bin/bash
 
+printf "\nThis script generates a unique IPv6 address based on the current timestamp and machine-id.\n"
+
 generate_ipv6() {
+
+  read -rp "Enter IPv6 prefix (default fd00:1abc): " prefix
+  prefix=${prefix:-fd00:1abc:}
+
   timestamp="$(date +%s%N)"
+
   uniqueid="$(cat /etc/machine-id)"
-  
+
   hash=$(printf "$timestamp$uniqueid" | sha1sum)
-  address=$(printf "$hash" | cut -c 1-32)
-  
-  formatted=$(awk 'BEGIN{OFS=FS=":"}{gsub(/..../,"&:")}1' <<< "$address")
 
-  prefix="fd00:1abc:"
+  address=$(printf "$hash" | cut -c 1-24)
 
-  echo "$prefix$formatted"
-}
+  tempfile=$(mktemp)
+  echo "$address" > "$tempfile"
+
+  formatted=$(awk 'BEGIN{OFS=FS=":"}{gsub(/..../,"&:")}1' "$tempfile")
+
+  rm "$tempfile"
+
+  echo "${prefix}${formatted%:}"
+
+  }
 
 address=$(generate_ipv6)
 
-echo "Unique IPv6 Address based on 'machine-id' and current timestamp: $address"
+echo "\nUnique IPv6 Address: \n$address\n"
